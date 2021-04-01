c26c4d60922c6a2949bfc2e0782ec527c5803a1f
andrymoran <65819107+andrymoran@users.noreply.github.com>
2021-04-01 20:16:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,14 @@
 	echo "Method not allowed";
 
 
+<script src="https://www.gstatic.com/dialogflow-console/fast/messenger-cx/bootstrap.js?v=1"></script>
+<df-messenger
+  df-cx="true"
+  location="us-central1"
+  chat-title="Itau"
+  agent-id="54fa59d5-18b5-4e58-b4e5-3ffdaf1cd82e"
+  language-code="es"
+></df-messenger>
+
 
 ?>
